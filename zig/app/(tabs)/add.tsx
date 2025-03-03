dc253c551539476c4ccb7cecfac7bef101c1b07f
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-03 19:49:53 -0300

    feat: adicionando caixas de diálogo no formulário

--- zig/app/(tabs)/add.tsx
+++ zig/app/(tabs)/add.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
-import { Alert, Text, TextInput, ScrollView, TouchableOpacity } from "react-native";
-import { Button, Card, IconButton } from 'react-native-paper';
+import { Alert, ScrollView } from "react-native";
+import { Button, Card, IconButton, Dialog, Portal, Provider, Text, TextInput } from 'react-native-paper';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as ImagePicker from 'expo-image-picker';
 
@@ -10,10 +10,11 @@ export default function AddEventScreen() {
 	const [startDate, setStartDate] = useState((new Date()));
 	const [endDate, setEndDate] = useState((new Date(startDate.getTime() + 60 * 60 * 1000)));
 	const [showPicker, setShowPicker] = useState({ active: false, type: null });
-	const [capacity, setCapacity] = useState('');
 	const [location, setLocation] = useState('');
 	const [description, setDescription] = useState('');
 	const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+	const [visible, setVisible] = useState(false);
+	const [dialogType, setDialogType] = useState<'title' | 'location' | 'description' | null>(null);
 
 	useEffect(() => {
 		(async () => {
@@ -36,10 +37,7 @@ export default function AddEventScreen() {
 		});
 
 		if (!result.canceled) {
-			setImages((prevImages) => [
-				...prevImages,
-				...result.assets.map((asset) => asset.uri),
-			]);
+			setImages((prevImages) => [...prevImages, ...result.assets.map((asset) => asset.uri)]);
 		}
 	};
 
@@ -62,6 +60,45 @@ export default function AddEventScreen() {
 
 	const formatDate = (date) => date.toLocaleString();
 
+	const showDialog = (type) => {
+		setDialogType(type);
+		setVisible(true);
+	};
+
+	const hideDialog = () => {
+		setDialogType(null);
+		setVisible(false);
+	};
+
+	const getValue = () => {
+		switch (dialogType) {
+			case 'title':
+				return title || '';
+			case 'location':
+				return location || '';
+			case 'description':
+				return description || '';
+			default:
+				return '';
+		}
+	};
+
+	const setValue = (text) => {
+		switch (dialogType) {
+			case 'title':
+				setTitle(text);
+				break;
+			case 'location':
+				setLocation(text);
+				break;
+			case 'description':
+				setDescription(text);
+				break;
+			default:
+				break;
+		}
+	};
+
 	return (
 		<ScrollView style={{ padding: 20 }}>
 			<Text style={{ fontSize: 24, marginBottom: 10 }}>Novo Rolê</Text>
@@ -74,14 +111,11 @@ export default function AddEventScreen() {
 
 			<IconButton icon="image-plus" size={24} onPress={pickImage} />
 
-			<TextInput
-				placeholder="Título"
-				value={title}
-				onChangeText={setTitle}
-				style={{ borderBottomWidth: 1, marginBottom: 20, marginTop: 20 }}
-			/>
+			<Button mode="outlined" onPress={() => showDialog('title')}>
+				{title || 'Adicionar Título'}
+			</Button>
 
-			<Button mode="outlined" onPress={() => showDatePicker('start')}>
+			<Button mode="outlined" onPress={() => showDatePicker('start')} style={{ marginTop: 10 }}>
 				Começo: {formatDate(startDate)}
 			</Button>
 
@@ -98,17 +132,41 @@ export default function AddEventScreen() {
 				/>
 			)}
 
-			<Button icon="map-marker" mode="outlined" onPress={() => { }} style={{ marginTop: 10 }}>
-				Adicionar Local
+			<Button icon="map-marker" mode="outlined" onPress={() => showDialog('location')} style={{ marginTop: 10 }}>
+				{location || 'Adicionar Localização'}
 			</Button>
 
-			<Button icon="file-document" mode="outlined" onPress={() => { }} style={{ marginTop: 10 }}>
-				Adicionar Descrição
+			<Button icon="file-document" mode="outlined" onPress={() => showDialog('description')} style={{ marginTop: 10 }}>
+				{description || 'Adicionar Descrição'}
 			</Button>
 
 			<Button mode="contained" style={{ marginTop: 10 }}>
 				Criar Rolê
 			</Button>
+
+			<Dialog visible={visible} onDismiss={() => setVisible}>
+				<Dialog.Title>
+					{dialogType === 'title' ? 'Editar título' :
+						dialogType === 'location' ? 'Adicionar local' :
+							'Adicionar Descrição'}
+				</Dialog.Title>
+
+				<Dialog.Content>
+					<TextInput
+						placeholder={
+							dialogType === 'title' ? 'Digite o título' :
+								dialogType === 'location' ? 'Digite o local' :
+									'Digite a descrição'}
+						value={getValue()}
+						onChangeText={setValue}
+					/>
+				</Dialog.Content>
+
+				<Dialog.Actions>
+					<Button onPress={hideDialog}>Cancelar</Button>
+					<Button onPress={hideDialog}>Salvar</Button>
+				</Dialog.Actions>
+			</Dialog>
 
 		</ScrollView>
 	);
