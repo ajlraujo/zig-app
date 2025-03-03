28474aae5c975840ca7f18bda5f6b240f9681e77
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-03 14:18:19 -0300

    feat: criar layout base do formulário (título, descrição, local)

--- zig/app/(tabs)/add.tsx
+++ zig/app/(tabs)/add.tsx
@@ -1,21 +1,37 @@
-import { Ionicons } from "@expo/vector-icons";
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from 'react';
+import { View, Text, TextInput, ScrollView, TouchableOpacity } from "react-native";
+import { Button, Card } from 'react-native-paper';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-export default function AddScreen() {
+
+export default function AddActvityScreen() {
+	const [title, setTitle] = useState('');
+	const [location, setLocation] = useState('');
+	const [description, setDescription] = useState('');
+
 	return (
-		<View style={styles.container}>
-			<Text style={styles.text}>Hello World!</Text>
-		</View>
-	);
-}
+		<ScrollView style={{ padding: 20 }}>
+			<Text style={{ fontSize: 24, marginBottom: 10 }}>Novo Rolê</Text>
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-	},
-	text: {
-		color: "#000"
-	}
-});
+			<TextInput
+				placeholder="Título"
+				value={title}
+				onChangeText={setTitle}
+				style={{ borderBottomWidth: 1, marginBottom: 20 }}
+			/>
+
+			<Button icon="map-marker" mode="outlined" onPress={() => { }} style={{ marginTop: 10 }}>
+				Adicionar Local
+			</Button>
+
+			<Button icon="file-document" mode="outlined" onPress={() => { }} style={{ marginTop: 10 }}>
+				Adicionar Descrição
+			</Button>
+
+			<Button mode="contained" style={{ marginTop: 10 }}>
+				Criar Rolê
+			</Button>
+
+		</ScrollView>
+	);
+};
