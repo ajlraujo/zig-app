3a5fe622f603808c3d670b1be6caa12e2e51fc3f
Julia Araujo <142631671+ajlraujo@users.noreply.github.com>
2025-03-09 13:58:37 -0300

    add: cria type de roteiros

---
+++ zig/types/events.ts
@@ -0,0 +1,11 @@
+export interface EventData {
+	title: string,
+	startDate: string,
+	endDate: string,
+	location: string,
+	description: string,
+	image: string;
+}
+
+export type DialogType = 'title' | 'location' | 'description';
+
